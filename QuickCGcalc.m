d879229e45c67459baceae4c5c9e965093c22d2d
Akash Patel <akash.patel.mae@gmail.com>
2015-05-31 17:59:12 -0700

    Revert "Everything"
    This reverts commit 999af154bf08ced52809f2fb49787161736c1331.

--- QuickCGcalc.m
+++
@@ -1,9 +0,0 @@
-wing_aft = 48.9;
-wing_forward = 47.25;
-wing_mac25 = 48.075;
-
-CG_forward = interp1(COG_npf(:,1),mac25,wing_forward,'linear');
-CG_aft = interp1(COG_npf(:,1),mac25,wing_aft,'linear');
-CG_mac25 = interp1(COG_npf(:,1),mac25,wing_mac25,'linear');
-
-nose2main = 54; %approx. of distance
